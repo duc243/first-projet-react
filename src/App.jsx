2ad86a5f780d58d0d4a050e3fb2d1da008241a7a
Exousia KASEKA <kasekaexousia@gmail.com>
2024-06-14 17:23:49 +0100

    creation d'un app de compteur

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Compteur from "./composant/Compteur";
 
 export default function App() {
-  const nom = "Exousia";
+  const nom = 5;
   return (
     <div>
       <Compteur nom={nom}/>
--- src/composant/Compteur.jsx
+++ src/composant/Compteur.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react"
 
 
-export default function Compteur(props) {
-    console.log(props);
+export default function Compteur() {
+  const [compteur, setCompteur] = useState(5)
+  const incremented = () => {
+    setCompteur( compteur + 1)
+  }
+  const decremented = () => {
+    setCompteur( compteur - 1)
+  }
   return (
     <div className="compteur">
-        <button>+</button>
-        <p>10</p>
-        <button>-</button>
+        <button onClick={incremented}>+</button>
+        <p className={ compteur < 0 && 'error'}>{compteur}</p>
+        <button onClick={decremented}>-</button>
     </div>
   )
 }
